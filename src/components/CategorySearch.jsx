0b986338c87fd6ca6ce2fdfbe6a6475546b74a04
alan-damian <alan41.dc@gmail.com>
2024-09-02 17:41:22 +0200

    commit 3?

--- src/components/CategorySearch.jsx
+++ src/components/CategorySearch.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import config  from '../API/config.js';
+import config from '../API/config.js';
 import './CategorySearch.module.css';
 
 const { apiUrl, apiKey } = config;
@@ -9,45 +9,46 @@ const { apiUrl, apiKey } = config;
 const CategorySearch = ({ category }) => {
   const language = 'en';
   const [news, setNews] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchNews = async () => {
       if (category !== '') {
-        const response = await axios.get(`${apiUrl}/top-headlines?category=${category}&language=${language}&apiKey=${apiKey}`);
-        setNews(response.data.articles);
+        try {
+          const response = await axios.get(`${apiUrl}/top-headlines?category=${category}&language=${language}&apiKey=${apiKey}`);
+          if (response.status === 200) {
+            setNews(response.data.articles);
+          } else {
+            setError('Error al obtener noticias');
+          }
+        // eslint-disable-next-line no-unused-vars
+        } catch (error) {
+          setError('Error al obtener noticias');
+        }
       }
     };
     fetchNews();
   }, [category]);
 
-
   return (
     <div>
-    
       <h2>{category} news</h2>
-      
-      <ul>
-        {news.map((article) => (
-          
-          <li key={article.title}>
-
-
-
-            <div className="card" >
-              <div className="card-body">
-                <h5 className="card-title">{article.title}</h5>
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <ul>
+          {news.map((article, index) => (
+            <li key={index}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{article.title}</h5>
+                </div>
               </div>
-            </div>
-
-
-
-
-
-
-            <h3>{article.author}</h3>
-          </li>
-        ))}
-      </ul>
+              <h3>{article.author}</h3>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
