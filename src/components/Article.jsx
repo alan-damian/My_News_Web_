7ede1467c1ff3a715cdd0f448080821d4cb98340
alan-damian <alan41.dc@gmail.com>
2024-09-02 21:13:21 +0200

    version 1

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
 
 function Article(props) {
+  if (!props.article) {
+    throw new Error('Article prop is required and cannot be null or undefined');
+  }
+
   return (
     <div>
       <h2>{props.article.title}</h2>
@@ -13,7 +17,7 @@ Article.propTypes = {
   article: PropTypes.shape({
     title: PropTypes.string.isRequired,
     description: PropTypes.string // Remove isRequired if description is optional
-  })
+  }).isRequired
 };
 
 export default Article;
